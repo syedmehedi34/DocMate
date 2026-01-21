ffc18549ac36cef517d0f4d3bede878f627baf2c
syedmehedi34 <syedmehedi34@gmail.com>
2026-01-21 21:31:19 +0600

    about section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import BannerSection from "./components/BannerSection";
 import Updates from "./components/Updates";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <div>
       <BannerSection />
       <Updates />
+      <About/>
     </div>
   );
 }
