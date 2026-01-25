6fff61e6d0826eb20aa38e34c1873fb9808780ea
syedmehedi34 <syedmehedi34@gmail.com>
2026-01-25 10:39:18 +0600

    about section polished

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,15 +2,15 @@
 
 import Link from "next/link";
 import React, { useEffect } from "react";
+import Image from "next/image";
 import AOS from "aos";
-import "aos/dist/aos.css"; // Import the AOS styles
+import "aos/dist/aos.css";
 
 const About = () => {
   useEffect(() => {
-    // Initialize AOS
     AOS.init({
-      duration: 1000, // Optional: Set the duration for the fade-up animation
-      once: true, // Optional: Make sure the animation only happens once
+      duration: 1000, // Animation duration in ms
+      once: true, // Animate only once
     });
   }, []);
 
@@ -22,35 +22,44 @@ const About = () => {
           className="flex justify-center md:col-span-2 md:row-span-3"
           data-aos="fade-up"
         >
-          <img
-            src="assets/abouts-img1.jpg"
+          <Image
+            src="/assets/abouts-img1.jpg"
+            alt="About Image 1"
+            width={500}
+            height={400}
             className="w-full h-50 md:h-full object-cover md:rounded-2xl"
-            alt=""
           />
         </div>
+
         {/* Short Image 2 */}
         <div
           className="flex justify-center md:col-span-2 md:row-span-3 md:col-start-1 md:row-start-4"
           data-aos="fade-up"
         >
-          <img
-            src="assets/abouts-img1.jpg"
+          <Image
+            src="/assets/abouts-img1.jpg"
+            alt="About Image 2"
+            width={500}
+            height={400}
             className="w-full h-50 md:h-full object-cover md:rounded-2xl"
-            alt=""
           />
         </div>
+
         {/* Long Image */}
         <div
           className="flex justify-center md:col-span-2 md:row-span-6 md:col-start-3 md:row-start-1"
           data-aos="fade-up"
         >
-          <img
-            src="assets/abouts-img2.jpg"
+          <Image
+            src="/assets/abouts-img2.jpg"
+            alt="Long About Image"
+            width={500}
+            height={800}
             className="w-full h-50 md:h-full object-cover md:rounded-2xl"
-            alt=""
           />
         </div>
-        {/* Description */}
+
+        {/* Description Section */}
         <div
           className="text-left p-4 md:p-8 md:col-span-3 md:row-span-6 md:col-start-5 md:row-start-1 flex flex-col justify-center"
           data-aos="fade-up"
@@ -66,13 +75,17 @@ const About = () => {
             providing African patients with world-class healthcare at affordable
             prices.
           </p>
-          <div className="flex flex-col md:flex-row justify-between items-center">
+
+          <div className="flex flex-col md:flex-row justify-between items-center mt-4">
+            {/* Features List */}
             <ul className="text-gray-700 space-y-2 md:space-y-0">
               <li>We Have Leading Edge Technology</li>
               <li>High Standards & Safety at Hospital</li>
               <li>We Have Professionals In Health</li>
               <li>Specialists In Diseases & Diagnosis</li>
             </ul>
+
+            {/* Years of Experience */}
             <div className="bg-[#0EA5E9] p-4 text-center text-white font-bold rounded-lg mt-4 md:mt-0">
               <h2 className="text-4xl md:text-5xl my-0">
                 12
@@ -81,6 +94,8 @@ const About = () => {
               </h2>
             </div>
           </div>
+
+          {/* More About Us Button */}
           <Link
             href="/pages/about"
             className="btn bg-[#0EA5E9] hover:bg-[#6dacc9] text-white mt-5 px-4 py-2 rounded-lg"
