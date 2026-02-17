c9c549b38dcafeecbdb1d87e937d8f5809ab2710
syedmehedi34 <syedmehedi34@gmail.com>
2026-02-18 00:33:52 +0600

    authentication updated

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,15 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.ibb.co.com",
+        hostname: "i.ibb.co",
+        // port: "",           // optional, default empty
+        // pathname: "/**",    // optional, সব path allow করতে (default /**)
       },
+      // যদি আরও কোনো domain থাকে (যেমন Google profile pic বা অন্য CDN) তাহলে যোগ করো
+      // {
+      //   protocol: "https",
+      //   hostname: "lh3.googleusercontent.com",
+      // },
     ],
   },
 };
