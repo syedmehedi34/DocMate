80224fa1d66f79c846a9a2f044ab3fba0741d74e
syedmehedi34 <syedmehedi34@gmail.com>
2026-03-19 00:10:22 +0600

    layout moved

--- src/app/dashboard/DashboardLayout.jsx
+++ src/app/dashboard/DashboardLayout.jsx
@@ -3,8 +3,8 @@
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import Sidebar from "./Sidebar";
-import Topbar from "./Topbar";
+import Sidebar from "../../components/Sidebar";
+import Topbar from "../../components/Topbar";
 
 export default function DashboardLayout({ children }) {
   const { data: session, status } = useSession();
