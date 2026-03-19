1578767f64fc3df26f99050fd43c6a30ee97fc17
syedmehedi34 <syedmehedi34@gmail.com>
2026-03-19 14:56:52 +0600

    eslint issue solved

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import pkg from "@eslint/eslintrc";
+
+const { FlatCompat } = pkg;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
